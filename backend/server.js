d81661848b505d4d29aebda290394fa29cd9bc49
Peter Tran <petertranfl@gmail.com>
2021-05-03 14:00:34 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,8 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
-
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
 app.use('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 })
