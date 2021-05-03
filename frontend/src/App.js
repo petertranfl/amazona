a07184bf40a06dc04340d2b52e63ffda2c41fc51
Peter Tran <petertranfl@gmail.com>
2021-05-03 13:29:39 -0500

    first commit for heroku publishing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,91 @@
-import React from 'react'
-import data from './data'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen'
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin
+  const signoutHandler = () => {
+    dispatch(signout())
+  }
+
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-            <a href="/" className="brand">amazona</a>
-        </div>
-        <div>
-            <a href="/cart.html">Cart</a>
-            <a href="/signin.html">Sign In</a>
-        </div>
-      </header>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link to="/" className="brand">
+              amazona
+              </Link>
+          </div>
+          <div>
+            <Link to="/cart">Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">
+                    {userInfo.name} <i className="fa fa-caret-down"></i>
+                  </Link>
+                  <ul className="dropdown-content">
+                    <li>
+                      <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                      <Link to="/orderhistory">Order History</Link>
+                    </li>
+                    <li>
+                      <Link to="#signout" onClick={signoutHandler}>
+                        Sign Out
+                        </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <Link to="/signin">Sign In</Link>
+              )
+            }
+          </div>
+        </header>
         <main>
-            <div className="row center">
-              {
-                data.products.map(product => (
-                  <div key={product._id} className="card">
-                        <a href={`/product/${product._id}`}>
-                            <img className="medium" src={product.image} alt={product.name}/>
-                        </a>
-                        <div className="card-body">
-                            <a href={`/product/${product._id}`}>
-                                <h2>{product.name}</h2>
-                            </a>
-                            <div className="rating">
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fas fa-star-half-alt"></i></span>
-                            </div>
-                            <div className="price">${product.price}</div>
-                        </div>
-                    </div>
-                ))
-              }
-            </div>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <PrivateRoute path="/profile" component={ProfileScreen}></PrivateRoute>
         </main>
         <footer className="row center">
-            All rights reserved.
-        </footer>
-    </div>
+          All rights reserved.
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
